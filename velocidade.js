ca5b10ddb278120086947d0cfa0746ba8d8ae986
kamikazeuchiha <126670951+kamikazeuchiha@users.noreply.github.com>
2023-10-16 11:56:50 -0300

    Add files via upload

--- velocidade.js
+++ velocidade.js
@@ -1,25 +1,19 @@
-const fprm = document.querySelector("form")
+const form = document.querySelector("form")
 const resp1 = document.querySelector("h3")
-const resp2 = document.querySelector("h4")
-//cria um ouvinte
-form.addEdventListener("submit",(e)=>{e.preventDefault()
-//evitar envio do form
-const nome = form.inNome.value
-//obtém o nome
-const nota1 = Number(form.inVelocidadePermitida.value)
-//obtém a primeira nota
-const nota2 = Number(form.inVelocidadeCondutor.value)
-//obtém segunda nota
-//média das notas
-resp1.innerText = `Media Da Velocidade ${media.toFixed(2)}`
-//criar as condições
-if(media >=30 ){
-resp2.innerText = `Ultrapassou o limite de velocidade`
-resp2.style.color = "red"
-} else if (media >=70) {
-    resp2.innerText = `Sua velocidade esta no limite`
-    resp2.style.color = "blue"
-}
-})
 
+form,addEventListener("submit", (e)=>{
+    e.preventDefault()
 
+    const permitida = Number(form.inPermitida.value)
+    const condutor = Number(form.inCondutor.value)
+    let leve = permitida + (permitida * 0.2)
+    
+    if(condutor <= permitida){
+      resp.innerText = `Situação: Sem Multa`
+
+     } else if(condutor <= leve) {
+        resp.innerText = `Situação: Multa Leve`
+    }else {
+        resp.innerText = `Situação: Multa Grave`
+    }   
+    })
